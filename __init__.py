279622d64706d7f8d5f92066215cf6cf2ee61b0d
tbrandstetter <thomas@brandstetter.co.at>
2016-12-18 15:46:36 +0100

    add python header

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
 #########################################################################
 #  Copyright 2016 Thomas Brandstetter           thomas@brandstetter.co.at
 #########################################################################
